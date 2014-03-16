57ddb1ba7978a13092093083a55766723c8199e0
Bryan Devereaux <bdeverea@gmail.com>
2014-03-16 02:32:26 -0400

    Added temp charts to UI to prep layout and implementation.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,12 @@
 class DashboardsController < ApplicationController
 
   def index
-
+    @events = Event.ytd_attendance(Date.today)
+    @chart_title = "YTD Attendance"
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
 end
